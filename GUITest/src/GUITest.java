94f52656c84761de24ae0a0625ad3d373bf8717b
Sam <chicagomike999@gmail.com>
2018-04-02 10:39:13 -0400

    Adding GUI Testing initial commit
    It creates a frame yee haw.

---
+++ GUITest/src/GUITest.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class GUITest {
+
+	public GUITest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		JFrame myFrame = new JFrame("Test frame please ignore");
+		myFrame.setSize(600, 300);
+		myFrame.setVisible(true);
+	}
+
+}
